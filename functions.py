35a991589bbdd50c9d72c642fa4cc10dec82ae8e
TipuatGit <87166372+TipuatGit@users.noreply.github.com>
2023-04-01 23:41:32 +0500

    Update current_habits() function
    -move main() function below help() function.
    - add functionality to current_habits() function, now it can:
        > read habit data from database
        > display habit data
    - add  functionality to add_new_habit() function, now it can:
        > user can add habit type such as 'daily' or 'weekly'
        > user types 'add' to add new habit otherwise empty sub-menu interface
        > help functionality modified

--- functions.py
+++ functions.py
@@ -3,16 +3,6 @@ def cls():
     import subprocess,os
     subprocess.call('cls' if os.name == 'nt' else 'clear', shell=True)
 
-def main():
-    '''Function prints the main interface of the program.'''
-    cls()
-    print("HABITS TRACKER!\n")
-
-    print('(1) Current Habits:')
-    print('(2) Add New Habit\n(3) View All Habits\n(4) My Progress')
-
-    print('\n-type \'h\' for help. \'q\' to quit.')
-
 
 #program closing function, can be needed to save files and then close program
 ##def close():
@@ -39,7 +29,8 @@ def help(interface = 'main'):
               detailed habit information.''')
 
     elif interface == 'add new habit':
-        print('add new habit help')
+        print("You can enter 'title', 'description', 'duration' and 'type' for your new habit. type 'add' to add new habit")
+        print('To cancel habit entry, leave all fields empty.\n')
 
     elif interface == 'view all habits':
         print('to go back, type "main"')
@@ -49,18 +40,63 @@ def help(interface = 'main'):
         print('to go back, type "main"')
         print('my progress help')
 
-    
+
+def main():
+    '''Function prints the main interface of the program.'''
+    cls()
+    print("HABITS TRACKER!\n")
+
+    print('(1) Current Habits:')
+    print('(2) Add New Habit\n(3) View All Habits\n(4) My Progress')
+
+    print('\n-type \'h\' for help. \'q\' to quit.')
+
 
 def current_habits():
 
+    #define function to retrieve data
+    def get_data():
+        import sqlite3
+        connection = sqlite3.connect('habit_db TEST.db')
+        cursor = connection.cursor()
+        cursor.execute('SELECT * FROM habits;')
+        return cursor.fetchall()
+        connection.close()
+        
     #clear screen to show sub-menu interface
     cls()
+
+    #output sub-menu heading
     print('CURRENT HABITS:\n')
-    print('(1) Walking | Duration: 0h:17m | 3')
-    print('(2) Newspaper | Duration: 1h:17m | 8')
-    print('(3) Cat feed | Coming up: 7h:3m | 6')
+
+    #define loop controlling variable
+    count = 0
+
+    #extract all attributes of a habit
+    #useful for taking the relevant attributes out separately
+    for (id,title,desc,duration,type,date) in get_data():
+        print(f'({count}) {title} | {duration} | ')
+        count += 1
+            
+##    print('(1) Walking | Duration: 0h:17m | 3')
+##    print('(2) Newspaper | Duration: 1h:17m | 8')
+##    print('(3) Cat feed | Coming up: 7h:3m | 6')
     print('\nto go back, type "main"')
 
+    i=''
+    while True:
+        i = input('>> ')
+        if i.isdigit():
+            if int(i) <= len(get_data()):
+                print(get_data()[int(i)])
+                
+            elif int(i) >= len(get_data()):
+                print('incorrect entry!')
+        elif i == 'q':
+            quit()
+        elif i == 'h':
+            help('current habits')
+                
 
 def add_new_habit():
     '''add_new_habit is a function that adds new habit
@@ -74,39 +110,50 @@ def add_new_habit():
     
     #define a variable to control the conditional statement
     confirm = 'n'
-
+    
+    print('ADD NEW HABIT:\n')
     #define while loop so in case user enters wrong info
     #they can fix it by repeating steps
     while True:
-        if confirm == 'n':
-            print('ADD NEW HABIT:\n')
-            print("You can enter 'title', 'description' and 'duration' for your new habit.")
-            print('To cancel habit entry, leave all fields empty.')
-            title = input('>> Title: ')
-            description = input('>> Description: ')
-            duration = input('>> Duration: ')
+        
+        print('Type \'h\' to see help.\n')
+        i = input('>> ')
+        if i == 'add':
+            if confirm == 'n':
+                
+                type = input('>> Type: ')
+                title = input('>> Title: ')
+                description = input('>> Description: ')
+                duration = input('>> Duration: ')
 
-            #define a way to cancel new habit entry
-            #due to simplicity of CLI, leaving empty fields
-            #should indicate to the program that habit entry
-            #is cancelled.
-            if title + description + duration == '':
-                print('\nProcess cancelled. No habit added.')
+                #define a way to cancel new habit entry.
+                #due to simplicity of CLI, leaving empty fields
+                #should indicate to the program that habit entry
+                #is cancelled.
+                if type + title + description + duration == '':
+                    print('\nProcess cancelled. No habit added.')
+                    break
+                else:
+                    #define confirmation check to let user
+                    #recheck if information is entered correctly.
+                    confirm = input('Confirm details (y/n)? ')
+                
+            elif confirm == 'y':
+                print('\nNew habit added!\nYour new habit is being tracked.')
                 break
-            else:
-                #define confirmation check to let user
-                #recheck if information is entered correctly.
-                confirm = input('Confirm details (y/n)? ')
-            
-        elif confirm == 'y':
-            print('\nNew habit added!\nYour new habit is being tracked.')
-            break
 
-        #define a check if user enters values other than expected
-        #to prevent breaking the program (infinite loop)
-        elif confirm == str():
-            print('\nEnter valid information.\n')
-            confirm = 'n'
+            #define a check if user enters values other than expected
+            #to prevent breaking the program (infinite loop)
+            elif confirm == str():
+                print('\nEnter valid information.\n')
+                confirm = 'n'
+
+        elif i == 'h':
+            help('add new habit')
+            
+        else:
+            main()
+            break
     
 
 def view_habits():
