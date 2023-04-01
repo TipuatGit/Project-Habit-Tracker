2a5313d74d0686b22e6b469997cb6a9d48f1491c
TipuatGit <87166372+TipuatGit@users.noreply.github.com>
2023-04-01 23:48:34 +0500

    Update habit class.py
    -add more variables to class __init__() function.
    -update add_to_database() method
    -add show_data() method for testing purpose
    -add info() method for testing purpose
    -automatically call class methods with class object definition
    -change database to a test database

--- habit class.py
+++ habit class.py
@@ -1,13 +1,61 @@
 import sqlite3
+import datetime
 
 class Habit:
-    def __init__(self, title, description, duration):
+    def __init__(self, title, description, duration, habit_type):
         self.title = title
         self.description = description
-        self.duration = duration
+        self.duration = duration # amount of time within which a habit task must be completed
+        self.habit_type = habit_type
+        self.creation_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+        #automatically call methods as soon as class object is created.
+        self.truncate_time()
+        self.add_to_database()
+
+    
 
     def add_to_database(self):
-        self.connection = sqlite3.connect('habit_db')
+        self.connection = sqlite3.connect('habit_db TEST.db')
         self.cursor = self.connection.cursor()
-        self.cursor.execute(f"INSERT INTO habits VALUES (?, ?, ?)",
-                            (self.title, self.description, self.duration))
+        self.cursor.execute(f"INSERT INTO habits VALUES (?, ?, ?, ?, ?, ?)",
+                            (None, self.title, self.description, self.duration,
+                             self.habit_type, self.creation_time)
+                            )
+        self.connection.commit()
+        self.connection.close()
+
+    def show_data(self):
+        self.connection = sqlite3.connect('habit_db TEST.db')
+        self.cursor = self.connection.cursor()
+        self.cursor.execute('SELECT * FROM habits;')
+        print(self.cursor.fetchall())
+        self.connection.close()
+        return self.cursor.fetchall()
+        
+    # a simple getter function 
+    def info(self):
+        print('Title:', self.title)
+        print('Description:', self.description)
+        print('Duration:', self.duration)
+        print('Habit Type:', self.habit_type)
+        print('Creation Time:', self.creation_time)
+
+    # test function to change init property values
+##    def truncate_time(self):
+##        if 'minute' in self.duration:
+##            self.duration = self.duration.split()
+##            self.duration[1] = 'm'
+##            self.duration = self.duration[0]+self.duration[1]
+
+##    def complete_task(self)
+
+
+# checking to see if things work...
+watering = Habit('Water Plants',None,'30 minutes', 'weekly')
+w = watering
+
+
+w.info()
+print()
+w.show_data()
