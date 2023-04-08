655b4606d5b57340641c715936bf15d976e4dad4
TipuatGit <87166372+TipuatGit@users.noreply.github.com>
2023-04-08 05:45:35 +0500

    remove info() and some tester code

--- habit class.py
+++ habit class.py
@@ -2,15 +2,16 @@ import sqlite3
 import datetime
 
 class Habit:
-    def __init__(self, title, description, duration, habit_type):
+    def __init__(self, title, description, time_start, time_end,habit_type):
         self.title = title
         self.description = description
-        self.duration = duration # amount of time within which a habit task must be completed
+        self.time_start = time_start # amount of time within which a habit task must be completed
+        self.time_end = time_end
         self.habit_type = habit_type
         self.creation_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
         #automatically call methods as soon as class object is created.
-        self.truncate_time()
+        #self.truncate_time()
         self.add_to_database()
 
     
@@ -18,9 +19,9 @@ class Habit:
     def add_to_database(self):
         self.connection = sqlite3.connect('habit_db TEST.db')
         self.cursor = self.connection.cursor()
-        self.cursor.execute(f"INSERT INTO habits VALUES (?, ?, ?, ?, ?, ?)",
-                            (None, self.title, self.description, self.duration,
-                             self.habit_type, self.creation_time)
+        self.cursor.execute(f"INSERT INTO habits VALUES (?, ?, ?, ?, ?, ?, ?)",
+                            (None, self.title, self.description, self.time_start,
+                             self.time_end, self.habit_type, self.creation_time)
                             )
         self.connection.commit()
         self.connection.close()
@@ -33,13 +34,6 @@ class Habit:
         self.connection.close()
         return self.cursor.fetchall()
         
-    # a simple getter function 
-    def info(self):
-        print('Title:', self.title)
-        print('Description:', self.description)
-        print('Duration:', self.duration)
-        print('Habit Type:', self.habit_type)
-        print('Creation Time:', self.creation_time)
 
     # test function to change init property values
 ##    def truncate_time(self):
@@ -50,12 +44,3 @@ class Habit:
 
 ##    def complete_task(self)
 
-
-# checking to see if things work...
-watering = Habit('Water Plants',None,'30 minutes', 'weekly')
-w = watering
-
-
-w.info()
-print()
-w.show_data()
