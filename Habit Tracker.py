eafd80a2c1450880c20ddee5155e0f1d27a117db
TipuatGit <87166372+TipuatGit@users.noreply.github.com>
2023-04-11 00:06:44 +0500

    remove interface variable
    this variable was causing problems with the help function. returning on main menu and getting help would not return main interface's help but the previous sub-menu's help.

--- Habit Tracker.py
+++ Habit Tracker.py
@@ -2,32 +2,22 @@ from functions import *
 
 main()
 
-interface = 'main'
-
 while True:
     user = input('>> ')
     if user=='h':
-        help(interface)
+        help()
         
     elif user=='q':
         quit()
-
-    elif user=='main':
-        main()
-        interface = 'main'
         
     elif user=='1' or user.lower() == "Current Habits".lower():
         current_habits()
-        interface = 'current habits'
-        
+                
     elif user=='2' or user.lower() == 'Add New Habit':
         add_new_habit()
-        interface = 'add new habit'
         
     elif user=='3' or user.lower() == 'View All Habits'.lower():
-        view_habits()
-        interface = 'view all habits'
+        view_all_habits()
         
     elif user=='4' or user.lower() == 'My Progress'.lower():
-        progress()
-        interface = 'my progress'
+        my_progress()
