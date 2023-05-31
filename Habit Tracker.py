fee27f1b690244f4aea1b5584556a7f0c30f03ca
TipuatGit <87166372+TipuatGit@users.noreply.github.com>
2023-05-31 14:45:29 +0500

    Update Habit Tracker.py
    -Remove code that checks for missed habits.
    -Move this code to functions.py file under a new function called check_missed_habits().
    -call function directly under while loop to constantly check and automatically update status for habits.

--- Habit Tracker.py
+++ Habit Tracker.py
@@ -4,41 +4,7 @@ from functions import *
 main()
 
 while True:
-    
-    current_time = datetime.datetime.now().time()
-
-    habits_table, _ = get_data()
-
-    for (id, title, desc, start, end, type, date) in habits_table:
-        end_time = datetime.datetime.strptime(end, '%H:%M:%S').time()
-        
-        if current_time > end_time:
-            connection = sqlite3.connect('habit_db TEST.db')
-            cursor = connection.cursor()
-            
-            # Check the habit type before entering 0 for missed habits
-            if type == 'daily':
-                query = "SELECT * FROM task_completion WHERE habit_id = ? AND DATE(completion_time) = DATE(?)"
-                cursor.execute(query, (id, datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")))
-                existing_record = cursor.fetchone()
-                
-                if not existing_record:
-                    query = "INSERT INTO task_completion (habit_id, completion_status, completion_time) VALUES (?,?,?)"
-                    cursor.execute(query, (id, 0, datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")))
-                    connection.commit()
-            
-            elif type == 'weekly':
-                query = "SELECT * FROM task_completion WHERE habit_id = ? AND strftime('%W', completion_time) = strftime('%W', ?)"
-                cursor.execute(query, (id, datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")))
-                existing_record = cursor.fetchone()
-                
-                if not existing_record:
-                    query = "INSERT INTO task_completion (habit_id, completion_status, completion_time) VALUES (?,?,?)"
-                    cursor.execute(query, (id, 0, datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")))
-                    connection.commit()
-            
-            connection.close()
-
+    check_missed_habits()
         
     user = input('>> ')
     if user=='h':
