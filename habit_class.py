b7e7eebee70f60221af55c8e921eb32893b4cc48
TipuatGit <87166372+TipuatGit@users.noreply.github.com>
2023-07-07 01:10:14 +0500

    remove unnecessary function

--- habit_class.py
+++ habit_class.py
@@ -2,7 +2,7 @@ import sqlite3
 import datetime
 
 class Habit:
-    def __init__(self, title, description, time_start, time_end,habit_type):
+    def __init__(self, title, description, time_start, time_end, habit_type):
         self.title = title
         self.description = description
         self.time_start = time_start # amount of time within which a habit task must be completed
@@ -11,7 +11,6 @@ class Habit:
         self.creation_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
         #automatically call methods as soon as class object is created.
-        #self.truncate_time()
         self.add_to_database()
 
 
@@ -24,14 +23,3 @@ class Habit:
                             )
         self.connection.commit()
         self.connection.close()
-        
-
-    # test function to change init property values
-##    def truncate_time(self):
-##        if 'minute' in self.duration:
-##            self.duration = self.duration.split()
-##            self.duration[1] = 'm'
-##            self.duration = self.duration[0]+self.duration[1]
-
-##    def complete_task(self)
-
